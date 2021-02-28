21b791aa4c0ef77b0ef346953364510323d0a7ed
Herivelton Andreassa <handreassa@gmail.com>
2021-02-28 13:32:42 -0300

    Database connectivity and user authentication

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,15 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+login = LoginManager(app)
+# Feature that forces user to be redirected to the login page before they view some pages
+login.login_view = 'login'
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,57 @@
-from flask import Flask, render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import Flask, render_template, flash, redirect, url_for, current_app
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from flask import request
+from werkzeug.urls import url_parse
+
+# To protect some page, use the decorator @login_required
 
 @app.route('/')
 @app.route('/home')
+@login_required
 def home():
-    user={'username': 'Herivelton'}
-    return render_template('home.html', title='Autenticação de usuário', user = user)
+    # posts = current_user.posts.all()
+    return render_template('home.html', title='Home page')
 
 @app.route('/about')
+@login_required
 def about():
     return render_template("about.html")
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('about'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username= form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congrats, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
